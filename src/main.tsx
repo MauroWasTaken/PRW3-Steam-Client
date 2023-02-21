0c3223f79152c8c81221bbf5abed1dae87c6c2c7
Nomeos <mathieu.rabot01@gmail.com>
2023-02-21 09:23:44 +0100

    Create a default react router with an outlet and the home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css'
+import Layout from "./pages/layout";
+import Home from "./pages/home";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container!);
+root.render(<App/>);
