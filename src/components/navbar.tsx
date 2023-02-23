fa4a7b58f07bf75a22ce9fc7b08818134b9130b1
Nomeos <mathieu.rabot01@gmail.com>
2023-02-23 16:24:16 +0100

    Create the navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import '/src/assets/style/navbar.css';
+
+export default function Navbar() {
+    return (
+        <div className={"nav_container"}>
+            <a className={"nav_title"}>
+                <h1>GameShelf</h1>
+            </a>
+        </div>
+    )
+}
