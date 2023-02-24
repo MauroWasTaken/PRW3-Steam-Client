501062d426ba2e0d9a1e2b0ee5c5d23e8de491ea
Nomeos <mathieu.rabot01@gmail.com>
2023-02-24 08:11:37 +0100

    change the card classname

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,11 +2,12 @@ import '/src/assets/style/card.css'
 
 export default function Card({game}: { game: any}) {
 
+
     return (
         <>
             <div className={"game"}>
-                <img src={game.splash} alt={game.name} className={"game_image"}/>
-                <div className={"game_title"}>
+                <img src={game.splash} alt={game.name} className={"image"}/>
+                <div className={"title"}>
                     <h2>{game.title}</h2>
                 </div>
             </div>
