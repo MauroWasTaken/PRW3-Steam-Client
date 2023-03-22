29e6571053482faf03043403d312086d15672969
Nomeos <mathieu.rabot01@gmail.com>
2023-03-22 12:58:59 +0100

    Add the buttons to the navbar and change the user on page reload

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,55 @@
 import '/src/assets/style/shared.css';
 import '/src/assets/style/navbar.css';
+import {useEffect, useState} from "react";
+import User from "../models/user";
+import {Button} from "primereact/button";
 
 export default function Navbar() {
+    const [user, setUser] = useState<User | null>();
+
+    // Check if the sessionStorage has been changed
+    useEffect(() => {
+        const user = sessionStorage.getItem('user');
+        if (user !== null) {
+            setUser(JSON.parse(user));
+        } else {
+            setUser(null);
+        }
+
+    }, [sessionStorage.getItem('user')]);
+
     return (
         <div className={"nav_container"}>
             <a className={"nav_title"} href='/'>
                 <h1>GameShelf</h1>
             </a>
+            <div className={"nav_buttons"}>
+                {user === null &&
+                    <>
+                        <Button className={"nav_button"} severity={'warning'} label={"Login"} icon={"pi pi-sign-in"}
+                                iconPos={"right"} onClick={() => {
+                            window.location.href = '/login'
+                        }}/>
+                        <Button className={"nav_button"} severity={'warning'} label={"Register"}
+                                icon={"pi pi-user-plus"}
+                                iconPos={"right"} onClick={() => {
+                            window.location.href = '/register'
+                        }
+                        }/>
+                    </>
+                }
+                {user !== null &&
+                    <>
+                        <Button icon="pi pi-user" className={"nav_button"} severity="warning" rounded
+                                aria-label="User"/>
+                        <Button className={"nav_button"} severity={'warning'} label={"Logout"} icon={"pi pi-sign-out"}
+                                iconPos={"right"} onClick={() => {
+                            sessionStorage.removeItem('user');
+                            window.location.href = '/'
+                        }}/>
+                    </>
+                }
+            </div>
         </div>
     )
 }
