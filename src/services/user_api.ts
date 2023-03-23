ae3aed352f8ec7ca811879a60a6752bc0069cbe7
Nomeos <mathieu.rabot01@gmail.com>
2023-03-23 12:51:55 +0100

    Create the api call when the wishlist is updated

--- src/services/user_api.ts
+++ src/services/user_api.ts
@@ -1,13 +1,21 @@
+import User from "../models/user";
+
 export default class UserApi {
-    public async registerUser(){
+    public async registerUser() {
 
     }
 
-    public async loginUser(){
+    public async loginUser() {
 
     }
 
-    public async updateUserWishlist(){
-
+    public async updateUserWishlist(user: any) {
+        return fetch("http://localhost:8493/users/" + user.id, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
     }
 }
