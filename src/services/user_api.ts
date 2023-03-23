f6156c44b6171e99c65245536b48113a67b91624
Nomeos <mathieu.rabot01@gmail.com>
2023-03-23 15:06:31 +0100

    Add the register and login function in the user service

--- src/services/user_api.ts
+++ src/services/user_api.ts
@@ -1,16 +1,28 @@
 import User from "../models/user";
 
 export default class UserApi {
-    public async registerUser() {
-
+    public async registerUser(user: { password: any; library: any[]; wishlist: any[]; email: any; username: any }) {
+        return fetch(import.meta.env.VITE_BACKEND_URL + "users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
     }
 
-    public async loginUser() {
-
+    public async loginUser(email: string, password: string) {
+        return fetch(import.meta.env.VITE_BACKEND_URL + "login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email, password}),
+        })
     }
 
     public async updateUserWishlist(user: any) {
-        return fetch("http://localhost:8493/users/" + user.id, {
+        return fetch(import.meta.env.VITE_BACKEND_URL + "users/" + user.id, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
